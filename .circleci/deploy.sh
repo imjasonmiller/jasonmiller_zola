ccc8dc55e066be4edd932481088ec616b8381be0
Jason Miller <contact@jasonmiller.nl>
2018-03-16 19:04:26 +0100

    Another deployment test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
+
+echo ${TEST}
 
 rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet" --quiet --omit-dir-times --update --delete --delay-updates ../public/ ${DEPLOY_ADR}
