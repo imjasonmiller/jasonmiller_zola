7e641bddb796f92f5e37ca55216a3e2cb818e688
Jason Miller <contact@jasonmiller.nl>
2018-03-16 18:25:30 +0100

    Test build

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet" --quiet --omit-dir-times --update --delete --delay-updates public/ ${DEPLOY_ADR}
+rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet" --quiet --omit-dir-times --update --delete --delay-updates public/ $DEPLOY_ADR
