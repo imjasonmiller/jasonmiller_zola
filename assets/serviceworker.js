dc5945e210afa0263ca95d802cb92a093b221e37
Jason Miller <contact@jasonmiller.nl>
2019-11-23 09:53:38 +0100

    Fix serviceworker on iOS

--- assets/serviceworker.js
+++ assets/serviceworker.js
@@ -3,37 +3,39 @@ const cacheName = "v1"
 // self.addEventListener("install", evt => {})
 
 self.addEventListener("activate", evt => {
-  // Remove unwanted caches
-  evt.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            return caches.delete(cache)
-          }
-        }),
-      ),
-    ),
-  )
+    // Remove unwanted caches
+    evt.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache)
+                    }
+                }),
+            ),
+        ),
+    )
 })
 
-const isCacheable = req => req.method === "GET" && /https?/.test(req.url)
+// const isCacheable = req => req.method === "GET" && /https?/.test(req.url)
+// Disable caching for now
+const isCacheable = req => false
 
 self.addEventListener("fetch", evt => {
-  evt.respondWith(
-    fetch(evt.request)
-      .then(res => {
-        if (isCacheable(evt.request)) {
-          const clone = res.clone()
+    evt.respondWith(
+        fetch(evt.request)
+            .then(res => {
+                if (isCacheable(evt.request)) {
+                    const clone = res.clone()
 
-          // Open and add to cache
-          caches.open(cacheName).then(cache => {
-            cache.put(evt.request, clone)
-          })
-        }
+                    // Open and add to cache
+                    caches.open(cacheName).then(cache => {
+                        cache.put(evt.request, clone)
+                    })
+                }
 
-        return res
-      })
-      .catch(err => caches.match(evt.request).then(res => res)),
-  )
+                return res
+            })
+            .catch(err => caches.match(evt.request).then(res => res)),
+    )
 })
