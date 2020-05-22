98389d5b0d6796bb1cabdd3597a3acc8ee7df02f
Jason Miller <contact@jasonmiller.nl>
2020-05-22 11:01:51 +0200

    Update serviceworker to work with generated files

--- assets/serviceworker.ts
+++ assets/serviceworker.ts
@@ -1,11 +1,9 @@
+// Allow use of `self`, see: https://github.com/microsoft/TypeScript/issues/14877
 export default null
 declare const self: ServiceWorkerGlobalScope
 
 const version = "v1"
 const cacheKey = `cache-${version}`
-
-const pageCache = ["/"]
-const fileCache = ["/js/vendor.js"]
 
 const handleInstall = async (event: ExtendableEvent): Promise<void> => {
     console.info("[sw] install")
@@ -16,15 +14,20 @@ const handleInstall = async (event: ExtendableEvent): Promise<void> => {
         (async (): Promise<void> => {
             const cache = await caches.open(cacheKey)
 
+            // Retrieve generated cache by assets/templates/cache.json
+            const {
+                data: { pages, files },
+            } = await fetch("/cache.json").then(response => response.json())
+
             await Promise.all(
-                [...pageCache, ...fileCache].map(async url => {
+                [...pages, ...files].map(async url => {
                     return cache.add(url).catch(err => {
                         console.error(`could not fetch ${url}, ${err}`)
                     })
                 }),
             )
 
-            console.log("saved caaaache")
+            console.info("[sw] saved pages and files to cache")
         })(),
     )
 }
@@ -32,23 +35,29 @@ const handleInstall = async (event: ExtendableEvent): Promise<void> => {
 const handleActivate = async (event: ExtendableEvent): Promise<void> => {
     console.info("[sw] activate")
 
+    await fetch("/cache.json")
+        .then(res => res.json())
+        .then(res => console.info("[sw] cache:", JSON.stringify(res, null, 2)))
+        .catch(err => console.error(err))
+
     const cacheKeys = await caches.keys()
     cacheKeys.filter(key => !key.includes(version)).forEach(caches.delete)
 
     self.clients.claim()
 }
 
-const dayPeriod = (hour: number): "morning" | "afternoon" | "evening" => {
+const dayPeriod = (hour: number): string => {
     if (hour < 12) return "morning"
     if (hour < 18) return "afternoon"
+
     return "evening"
 }
 
 // Changing the greet by Service Worker allows us to prevent
-// the flash of updated content when done via inline JavaScript.
+// the flash of changed content when using inline JavaScript.
 //
-// The downside is that the Service Worker can not modify requests before
-// install and thus a default greeting value is required.
+// The downside is that the Service Worker can not modify requests
+// before install and thus a default initial value is required.
 const renderIndex = async (
     match: Promise<Response | undefined>,
     // path: string,
@@ -77,10 +86,25 @@ const renderIndex = async (
     )
 }
 
+const getCache = (req: Request): Promise<Response> => {
+    return new Promise((resolve, reject) => {
+        caches
+            .match(req)
+            .then(res => {
+                if (res) {
+                    resolve(res)
+                } else {
+                    fetch(req).then(resolve, reject)
+                }
+            })
+            .catch(reject)
+    })
+}
+
 const handleFetch = async (event: FetchEvent): Promise<void> => {
     const { pathname, hostname } = new URL(event.request.url)
 
-    console.log("[sw] fetch", hostname, pathname)
+    console.info("[sw] fetch", hostname, pathname)
 
     // Do not fetch resources from other origins
     if (hostname !== self.location.hostname) {
@@ -90,9 +114,11 @@ const handleFetch = async (event: FetchEvent): Promise<void> => {
     // Pages
     if (event.request.headers.get("accept")?.includes("text/html")) {
         if (pathname === "/") {
-            event.respondWith(renderIndex(caches.match("/")))
+            return event.respondWith(renderIndex(caches.match("/")))
         }
     }
+
+    event.respondWith(getCache(event.request))
 }
 
 self.addEventListener("fetch", event => handleFetch(event))
